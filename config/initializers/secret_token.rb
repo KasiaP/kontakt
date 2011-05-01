639750eeb0741f87b060cd8b4a282ba90e5e5ca7
Kasia Paszkiewicz <katarzyna.j.paszkiewicz@gmail.com>
2011-05-01 11:09:00 +0200

    pierwsze puszniecie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kontakt::Application.config.secret_token = '4aa25c0847f6b664de1dc02a5fb1a1da5d6ce61f6d0ef1a6b11514a33412618c60de6990152e8298b37b28acb03e5a22fc807d626e732221b5dce8e7360a8db4'
