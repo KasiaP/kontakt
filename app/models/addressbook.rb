5c20e80ceb7e5a8318398666f8c33f3598165c37
Kasia Paszkiewicz <katarzyna.j.paszkiewicz@gmail.com>
2011-05-20 20:20:27 +0200

    koniec

--- app/models/addressbook.rb
+++ app/models/addressbook.rb
@@ -1,5 +1,6 @@
 class Addressbook < ActiveRecord::Base
-attr_accessible :imie, :nazwisko, :email
+attr_accessible :imie, :nazwisko, :email, :adres
+validates_presence_of :nazwisko, :imie, :email 
 def self.search(search)
   if search
     where('nazwisko LIKE ?', "%#{search}%")
